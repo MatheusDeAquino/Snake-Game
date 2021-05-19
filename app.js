7135a1aca8deeb928f5ae9f79bbee4775b3463b6
MatheusDeAquino <matheusaquinof@gmail.com>
2021-05-19 14:45:50 -0300

    add snake moviments rules

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function createArea(){
     context.fillStyle = "tan";
@@ -19,5 +20,27 @@ function createSnake(){
     }
 }
 
-createArea();
-createSnake();
+function startGame(){
+    createArea();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead ={
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+
+}
+
+let intervalGame = setInterval(startGame, 100);
